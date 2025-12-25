dc9862c915648afdc948cc8e984371da30b9a9a1
Nagavignesh1729 <mnaga.1729@gmail.com>
2025-12-24 23:37:18 -0800

    refactor: treating center as origin is confusing. World treats 0, 0 as top left

--- 2D/myworld.py
+++ 2D/myworld.py
@@ -1,9 +1,42 @@
+import pygame
+
 class MyWorld:
-    def __init__(self, screen, bodies):
-        self.screen = screen
+    def __init__(self, dimension, bodies, axes=False):
+        self.WIDTH, self.HEIGHT = dimension
+        self.center = (self.WIDTH // 2, self.HEIGHT // 2)
         self.bodies = bodies
+        
+        for body in self.bodies:
+            body.set_position_at_center(self.center)
+        
+        self.screen = pygame.display.set_mode(dimension)
+        self.axes = axes
     
+    def draw(self):
+        self.screen.fill((0, 0, 0))
+        if self.axes:
+            pygame.draw.line(self.screen, (100, 100, 100), (0, self.HEIGHT//2), (self.WIDTH, self.HEIGHT//2))
+            pygame.draw.line(self.screen, (100, 100, 100), (self.WIDTH//2, 0), (self.WIDTH//2, self.HEIGHT))
+        
+        for body in self.bodies:
+            body.draw(self.screen)
+            
     def update(self, dt):
         for body in self.bodies:
             body.update(dt)
-            body.draw(self.screen)
+            
+            lx_min, lx_max, ly_min, ly_max = body.bounds # local box
+            
+            if body.position.x + lx_min < 0:
+                body.velocity.x *= -1
+                body.position.x = -lx_min
+            elif body.position.x + lx_max > self.WIDTH:
+                body.velocity.x *= -1
+                body.position.x = -lx_max
+            
+            if body.position.y + ly_min < 0:
+                body.velocity.y *= -1
+                body.position.y = -ly_min
+            elif body.position.y + ly_max > self.WIDTH:
+                body.velocity.y *= -1
+                body.position.y = -ly_max
--- 2D/objects.py
+++ 2D/objects.py
@@ -9,6 +9,7 @@ class Body:
         self.shape = shape
         self.velocity = velocity
         self.rotation_speed = rotation_speed
+        self.bounds = self.set_local_boundary()
         
     def update(self, dt):
         self.position += self.velocity * dt
@@ -19,16 +20,24 @@ class Body:
         vertices = self.shape.get_vertices()
         res = []
         theta = (self.orientation * math.pi / 180)
-        rotate_vector = Vector(math.cos(theta), math.sin(theta)).normalize()
+        rotate_vector = Vector(math.cos(theta), math.sin(theta))
         
-        for i in range(len(vertices)):
-            x, y = (vertices[i][0], vertices[i][1])
-            point = Vector(x, y)
+        for v in vertices:
+            point = Vector(v[0], v[1])
             new_point = point.rotate_by(rotate_vector)
             res.append((self.position.x + new_point.x, self.position.y + new_point.y))
             
         pygame.draw.polygon(screen, self.shape.color, res)
 
+    def set_local_boundary(self):
+        verts = self.shape.get_vertices()
+        min_x = min(v[0] for v in verts)
+        max_x = max(v[0] for v in verts)
+        min_y = min(v[1] for v in verts)
+        max_y = max(v[1] for v in verts)
+        
+        return (min_x, max_x, min_y, max_y)
+    
 class Shape:
     def __init__(self, vertices=None, color = (255, 255, 255)):
         self.vertices = vertices
@@ -40,31 +49,34 @@ class Shape:
     
 class ShapeMaker:
     @staticmethod
-    def make_square(length):
+    def make_square(length, color):
         return ShapeMaker.helper_make_regular_polygon(
             4, 
             length/math.sqrt(2), 
-            math.pi/4
+            math.pi/4,
+            color
         )
     
     @staticmethod
-    def make_triangle(length):
+    def make_triangle(length, color):
         return ShapeMaker.helper_make_regular_polygon(
             3, 
             length/(2*math.sin(math.pi/3)), 
-            -math.pi/2
+            -math.pi/2,
+            color
         )
     
     @staticmethod
-    def make_regular_polygon(nsides, length, offset = 0):
+    def make_regular_polygon(nsides, length, offset=0, color=(255, 255, 255)):
         return ShapeMaker.helper_make_regular_polygon(
             nsides,
             length/(2*math.sin(math.pi/nsides)),
-            0                       
+            offset,
+            color                  
         )
         
     @staticmethod
-    def helper_make_regular_polygon(nverts, r, offset):
+    def helper_make_regular_polygon(nverts, r, offset, color):
         verts = []
         theta = offset
         angle = math.pi * 2 / nverts 
@@ -73,5 +85,6 @@ class ShapeMaker:
             theta += angle
         
         return Shape(
-            vertices = verts
+            vertices = verts,
+            color = color
         )
